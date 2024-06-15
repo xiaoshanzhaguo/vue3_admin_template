5b632704b6ddeb90efd421b37efa89ba51980a87
shanzhaguo <xiaoshanzhaguo@163.com>
2024-06-15 18:14:22 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="box">
+    <h1>App跟组件</h1>
+  </div>
+</template>
+<script setup lang="ts">
+const str = '我爱你祖国'
+</script>
+<style scoped lang="scss">
+.box {
+  width: 600px;
+  height: 400px;
+  background: black;
+  h1 {
+    color: white;
+  }
+}
+</style>
