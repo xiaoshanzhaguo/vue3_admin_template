a4b7b03db83801a75cc6c4b5971032816bbead96
shanzhaguo <xiaoshanzhaguo@163.com>
2024-06-19 23:12:00 +0800

    feat: layout组件的静态的搭建

---
+++ src/layout/index.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="layout_container">
+    <!-- 左侧菜单 -->
+    <div class="layout_slider">123</div>
+    <!-- 顶部导航 -->
+    <div class="layout_tabbar">456</div>
+    <!-- 内容展示区域 -->
+    <div class="layout_main">
+      <p style="height: 10000px; background: red">我是一个段落</p>
+    </div>
+  </div>
+</template>
+<script setup lang="ts"></script>
+<style scoped lang="scss">
+.layout_container {
+  width: 100%;
+  height: 100vh;
+  background: red;
+
+  .layout_slider {
+    width: $base-menu-width;
+    height: 100vh;
+    background: $base-menu-background;
+  }
+
+  .layout_tabbar {
+    position: fixed;
+    width: calc(100% - $base-menu-width);
+    height: $base-tabber-height;
+    background: cyan;
+    top: 0px;
+    left: $base-menu-width;
+  }
+
+  .layout_main {
+    position: absolute;
+    width: calc(100% - $base-menu-width);
+    height: calc(100vh - $base-tabber-height);
+    background: yellowgreen;
+    left: $base-menu-width;
+    top: $base-tabber-height;
+    padding: 20px;
+    overflow: auto;
+  }
+}
+</style>
