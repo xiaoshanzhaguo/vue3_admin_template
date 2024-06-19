94c82a7f08260d57ff12325162fb9154c779ac66
shanzhaguo <xiaoshanzhaguo@163.com>
2024-06-19 22:35:05 +0800

    feat: 自定义校验表单

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -3,7 +3,12 @@
     <el-row>
       <el-col :span="12" :xs="0"></el-col>
       <el-col :span="12" :xs="24">
-        <el-form class="login_form" :model="loginForm" :rules="rules" ref="loginForms">
+        <el-form
+          class="login_form"
+          :model="loginForm"
+          :rules="rules"
+          ref="loginForms"
+        >
           <h1>Hello</h1>
           <h2>欢迎来到硅谷甄选</h2>
           <el-form-item prop="username">
@@ -88,6 +93,26 @@ const login = async () => {
     })
   }
 }
+//自定义校验规则函数
+const validatorUsername = (rule: any, value: any, callback: any) => {
+  //rule:即为校验规则对象
+  //value:即为表单元素文本内容
+  //callback:函数，如果符合条件callBack放行通过，如果不符合条件callBack方法，注入错误提示信息
+  if (value.length >= 5) {
+    callback()
+  } else {
+    callback(new Error('账号长度至少五位'))
+  }
+}
+
+const validatorPassword = (rule: any, value: any, callback: any) => {
+  if (value.length >= 6) {
+    callback()
+  } else {
+    callback(new Error('密码长度至少六位'))
+  }
+}
+
 //定义表单校验需要配置的对象
 const rules = {
   //规则对象属性：
@@ -97,21 +122,29 @@ const rules = {
   //message:错误的提示信息
   //trigger:触发校验表单的时机 change->文本发生变化触发校验 blur->失去焦点的时候触发校验规则
   username: [
+    // {
+    //   required: true,
+    //   min: 6,
+    //   max: 10,
+    //   message: '账号长度至少6位',
+    //   trigger: 'change',
+    // },
     {
-      required: true,
-      min: 6,
-      max: 10,
-      message: '账号长度至少6位',
-      trigger: 'change',
+      trigger: 'blur',
+      validator: validatorUsername,
     },
   ],
   password: [
+    // {
+    //   required: true,
+    //   min: 6,
+    //   max: 15,
+    //   message: '密码的长度至少6位',
+    //   trigger: 'change',
+    // },
     {
-      required: true,
-      min: 6,
-      max: 15,
-      message: '密码的长度至少6位',
-      trigger: 'change',
+      trigger: 'blur',
+      validator: validatorPassword,
     },
   ],
 }
